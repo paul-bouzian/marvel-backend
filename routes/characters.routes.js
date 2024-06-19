eb3e5d384db13373bf360a5853a25a8fd95fefc3
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-19 13:26:48 +0200

    finished all routes

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -14,6 +14,27 @@ router.get("/characters", async (req, res) => {
 
     res.status(200).json(response.data);
   } catch (error) {
+    if (error.response.status === 400) {
+      return res.status(400).json({ message: "Bad request" });
+    }
+    res.status(500).json({ message: "Internal server error" });
+    console.log(error);
+  }
+});
+
+router.get("/character/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const requestUrl = `${process.env.MARVEL_API_URL}/character/${id}?apiKey=${process.env.MARVEL_API_KEY}`;
+
+    const response = await axios.get(requestUrl);
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    if (error.response.status === 400) {
+      return res.status(400).json({ message: "Character not found" });
+    }
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
   }
--- routes/comics.routes.js
+++ routes/comics.routes.js
@@ -14,6 +14,45 @@ router.get("/comics", async (req, res) => {
 
     res.status(200).json(response.data);
   } catch (error) {
+    if (error.response.status === 400) {
+      return res.status(400).json({ message: "Bad request" });
+    }
+    res.status(500).json({ message: "Internal server error" });
+    console.log(error);
+  }
+});
+
+router.get("/comic/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const requestUrl = `${process.env.MARVEL_API_URL}/comic/${id}?apiKey=${process.env.MARVEL_API_KEY}`;
+
+    const response = await axios.get(requestUrl);
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    if (error.response.status === 400) {
+      return res.status(400).json({ message: "Comic not found" });
+    }
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/comics/:characterId", async (req, res) => {
+  const { characterId } = req.params;
+
+  try {
+    const requestUrl = `${process.env.MARVEL_API_URL}/comics/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`;
+
+    const response = await axios.get(requestUrl);
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    if (error.response.status === 400) {
+      return res.status(400).json({ message: "Comics not found" });
+    }
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
   }
